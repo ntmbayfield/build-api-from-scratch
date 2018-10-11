faa6943511554e21838684ac13aadeb91483a957
Nyiema Bayfield <ntmbayfield@gmail.com>
2018-10-11 15:22:20 -0700

    Update app.js file to include router and error handling.

--- app.js
+++ app.js
@@ -4,3 +4,25 @@ const port = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const router = express.Router();
+
+app.disable('x-powered-by')
+if (process.env.NODE_ENV === 'development') app.use(morgan('dev'))
+app.use(bodyParser.json())
+
+const clothingRouter = require('./src/routes/clothing');
+
+app.use('/clothing', clothingRouter);
+
+app.use((err, req, res, next) => {
+  const status = err.status || 500
+  res.status(status).json({ error: err })
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({ error: { message: 'Not found' }})
+})
+
+const listener = () => `Listening on port ${port}!`
+app.listen(port, listener)
+
+module.exports = app
